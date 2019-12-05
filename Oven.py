b54c6bd713f589077058c95c6273e7e226895041
Bob Larsen <rwlarsen@gmail.com>
2019-12-05 12:37:30 -0800

    removed non-ascii characters

--- Oven.py
+++ Oven.py
@@ -2,8 +2,8 @@ import abc
 
 
 class Oven(abc.ABC):
-    def iń(self, i: int) -> int:
+    def input(self, i: int) -> int:
         pass
 
-    def out(self, i: int, b: bool) -> None:
+    def output(self, i: int, b: bool) -> None:
         pass
--- refactor_me.py
+++ refactor_me.py
@@ -8,12 +8,12 @@ def regulate_temp(self):
 
     while True:
         o: Oven = Oven()
-        goalT = o.iń(0x02)
-        t = o.iń(0x01)
-        s = o.iń(0x03)
+        goalT = o.input(0x02)
+        t = o.input(0x01)
+        s = o.input(0x03)
 
         if s == 1:
             if t < goalT:
-                o.out(0x04, True)
+                o.output(0x04, True)
             else:
-                o.out(0x04, False)
+                o.output(0x04, False)
